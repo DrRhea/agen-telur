e10cacfe7cf709fb486f37199c82ec4ed714c955
safiraaliyah <166832611+safiraaliyah@users.noreply.github.com>
2024-08-14 16:57:39 +0700

    feat(grosir):ubah halaman pesanan saya, beranda dan navbar

--- database/migrations/2024_08_03_205951_create_restock_trigger.php
+++ database/migrations/2024_08_03_205951_create_restock_trigger.php
@@ -11,24 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-      DB::unprepared('
-            CREATE TRIGGER restock_produk
-            AFTER UPDATE ON produks
-            FOR EACH ROW
-            BEGIN
-                DECLARE reorder_date DATE;
-                DECLARE total_required_stock INT;
-                SET reorder_date = DATE_ADD(CURDATE(), INTERVAL NEW.lead_time_produk DAY);
-                SET total_required_stock = NEW.rop_produk + NEW.safety_stock_produk;
 
-                IF NEW.stok_produk <= total_required_stock THEN
-                    UPDATE produks
-                    SET stok_produk = stok_produk + (NEW.rop_produk * 2),
-                        stok_realtime_produk = stok_realtime_produk + (NEW.rop_produk * 2)
-                    WHERE id = NEW.id;
-                END IF;
-            END;
-        ');
     }
 
     /**
@@ -36,6 +19,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::unprepared('DROP TRIGGER IF EXISTS restock_produk');
     }
 };
